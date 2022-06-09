5ebf2ec20d778cd5f48786992a99bc88e246582b
SormaniGabriele <g.sormani6@campus.unimib.it>
2022-06-09 17:59:09 +0200

    Commit 3
    Settate le rest di base e classe account

--- ProgettoSD-Banca/src/main/java/com/example/demo/DemoApplication.java
+++ ProgettoSD-Banca/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	static List<Account> accountList = new ArrayList<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		Account a = new Account("provsa", "prova");
+		accountList.add(a);
+		
 	}
 
 }
--- ProgettoSD-Banca/src/main/java/com/example/demo/ManageBanca.java
+++ ProgettoSD-Banca/src/main/java/com/example/demo/ManageBanca.java
@@ -1,8 +1,29 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ManageBanca {
 
+	@RequestMapping("/api/account")
+	public List<Account> returnAllAccount() {
+		
+		return DemoApplication.accountList;
+	}
+	
+	@RequestMapping(value = "/api/account", method = RequestMethod.POST)
+	public void CreaAccount(@RequestBody String account) {
+		
+		Map<String, String> body = new HashMap<>();
+		
+	}
 }
+
+
